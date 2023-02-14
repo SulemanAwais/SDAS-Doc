69a7b110013f4a81a734fe0ef68c249cc05bcbd9
SulemanAwais <102269316+SulemanAwais@users.noreply.github.com>
2023-02-14 06:16:13 +0500

    1.1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Sensor Data Acqusition System',
+  tagline: 'Sensor data from smart devices',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
